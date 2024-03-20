bc026a6a35e54c10fadd13a45e335d4c4cf13c15
Umesh Deshpande <udeshpa@us.ibm.com>
2024-03-20 03:17:15 -0400

    Baseline Test

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -1,22 +1,33 @@
 from vllm import LLM, SamplingParams
+import os
+import time
+import faulthandler
+faulthandler.enable()
 
-# Sample prompts.
 prompts = [
     "Hello, my name is",
     "The president of the United States is",
     "The capital of France is",
     "The future of AI is",
 ]
-# Create a sampling params object.
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
-# Create an LLM.
-llm = LLM(model="facebook/opt-125m")
-# Generate texts from the prompts. The output is a list of RequestOutput objects
-# that contain the prompt, generated text, and other information.
-outputs = llm.generate(prompts, sampling_params)
-# Print the outputs.
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+os.environ["CUDA_VISIBLE_DEVICES"] = "4"
+
+smoe = LLM(model="NickyNicky/Mixtral-4x1.1B-TinyDolphin-2.8-1.1b_oasst2_chatML_Cluster",
+          max_num_seqs=2, gpu_memory_utilization=0.25)
+
+start_time = time.time()
+for _ in range(10):
+    outputs = smoe.generate(prompts, sampling_params)
+end_time = time.time()
+
+print(f"Generated {len(outputs)} completions in {(end_time - start_time) / 10:.2f} seconds")
+
 for output in outputs:
     prompt = output.prompt
     generated_text = output.outputs[0].text
     print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+while True:
+    time.sleep(1000)
